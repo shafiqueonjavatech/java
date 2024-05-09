623ace0fa7fb07ae9ba4c1fcfbf29c0b43683314
shafiqueonjavatech <169106408+shafiqueonjavatech@users.noreply.github.com>
2024-05-09 13:08:32 +0200

    Update Main.java
    added a log stmt

--- src/main/java/org/shafique/Main.java
+++ src/main/java/org/shafique/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println( "Hello and welcome!" );
         for ( int i = 1; i <= 5; i++ ) {
             System.out.println ( "i value = " + i );
+            System.out.println("new statement");
         }
     }
 }
