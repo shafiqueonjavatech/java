a08db9c869f67ec45af248306765538e266d1bf3
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2024-05-09 20:32:23 +0200

    "updated loggings"

--- src/main/java/org/shafique/Main.java
+++ src/main/java/org/shafique/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println( "Hello and welcome!" );
         for ( int i = 1; i <= 5; i++ ) {
             System.out.println ( "current counter - i value = " + i );
+            System.out.println ("new stmt" );
         }
     }
 }
